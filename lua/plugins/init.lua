2eaad7f41be66c918dc89620ee468189b5794ada
Jakob Skov-Pedsen <jasp@thecrypt.dk>
2024-09-21 22:06:49 +0200

    Add crate management plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,5 +27,14 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
+  },
+
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    event = { "BufRead Cargo.toml" },
+    config = function()
+      require('crates').setup()
+    end,
   }
 }
