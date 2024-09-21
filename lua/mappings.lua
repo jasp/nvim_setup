270dd016bdce79dde889f347560a9296ab931241
Jakob Skov-Pedsen <jasp@thecrypt.dk>
2024-09-21 22:09:20 +0200

    Add default shortcuts for LSP

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,15 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "grn", function ()
+  vim.lsp.buf.rename()
+end)
+
+map("n", "gra", function ()
+  vim.lsp.buf.code_action()
+end)
+
+map("n", "grr", function ()
+  vim.lsp.buf.references()
+end)
