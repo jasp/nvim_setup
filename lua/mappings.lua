1f6e8b5154f6ae657fe1e601a0c596dfc215b733
Jakob Skov-Pedsen <jasp@thecrypt.dk>
2024-09-22 13:30:05 +0200

    Setup debugging with dap and codelldb

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,4 +12,26 @@ map("i", "jk", "<ESC>")
 map("n", "grn", vim.lsp.buf.rename)
 map("n", "gra", vim.lsp.buf.code_action)
 map("n", "grr", vim.lsp.buf.references)
+map("n", "gi", vim.lsp.buf.implementation)
+map("n", "<leader>f", function ()
+  vim.lsp.buf.format({async = true})
+end)
+
+-- Debugger commands
+-- yoink from here: https://github.com/letsgetrusty/neovim-rust/blob/main/lua/mappings.lua
+local dap = require('dap')
+map("n", "<Leader>dl", dap.step_into, { desc = "Debugger step into"})
+map("n", "<Leader>dj", dap.step_over, { desc = "Debugger step over"})
+map("n", "<Leader>dk", dap.step_out, { desc = "Debugger step out"})
+map("n", "<Leader>dc", dap.continue, { desc = "Debugger continue"})
+map("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Debugger tobble breakpoint"})
+map("n",
+  "<Leader>dd",
+  function ()
+    dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+  end,
+  { desc = "Debugger set conditional breakpoint"}
+)
+map("n", "<Leader>de", dap.terminate, { desc = "Debugger reset"})
+map("n", "<Leader>dr", dap.run_last, { desc = "Debugger run last"})
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,15 +28,32 @@ return {
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
     config = function(_,_)
+      local mason_registry = require('mason-registry')
+      local codelldb = mason_registry.get_package('codelldb')
+      local extension_path = codelldb:get_install_path() .. '/extension/'
+      local codelldb_path = extension_path .. 'adapter/codelldb'
+      local liblldb_path = extension_path .. 'lldb/lib/liblldb'
+      local this_os = vim.loop.os_uname().sysname
+
+      -- windows fixes
+      if this_os == 'Windows' then
+        extension_path = codelldb:get_install_path() .. '\\extension\\'
+        codelldb_path = extension_path .. "adapter\\codelldb.exe"
+        liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+      else
+        liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+      end
+
+      local cfg = require('rustaceanvim.config')
       vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
         tools = {
         },
         server = {
-          on_attach = function (client, bufnr)
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr })
-            vim.keymap.set("n", "<leader>f", function ()
-              vim.lsp.buf.format({async = true})
-            end, { buffer = bufnr })
+          on_attach = function (_, bufnr)
+            vim.keymap.set("n", "dt", "RustLsp testables", {buffer = bufnr})
           end,
         }
       }
@@ -50,5 +67,32 @@ return {
     config = function()
       require('crates').setup()
     end,
-  }
+  },
+
+  {
+    'mfussenegger/nvim-dap',
+    config = function ()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function ()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function ()
+        dapui.close()
+      end
+    end
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+      require("dapui").setup()
+    end,
+  },
 }
