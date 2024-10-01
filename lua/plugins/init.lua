770170e661cd5dd5508a0cc3c23bd8f48d3436d0
Jakob Skov-Pedersen <jasp@thecrypt.dk>
2024-10-01 14:22:13 +0200

    Add neotest for running/viewing tests

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -104,4 +104,21 @@ return {
       require("dapui").setup()
     end,
   },
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter"
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require('rustaceanvim.neotest')
+        },
+    }
+    end,
+  },
 }
