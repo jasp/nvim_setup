271b3635977ccdb79440d1b2e5cfb6925b1f04df
Jakob Skov-Pedersen <jasp@devicelab.dk>
2026-01-31 16:07:59 +0100

    Update to new lsp config method

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,26 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "csharp_ls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
+local servers = { "html", "cssls", "clangd", "csharp_ls", "rust_analyzer" }
+vim.lsp.enable(servers)
 vim.lsp.inlay_hint.enable()
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -71,11 +71,21 @@ return {
     event = { "BufRead Cargo.toml" },
     config = function()
       require('crates').setup {
-        completion = {
-          cmp = {
-            enabled = true
-          },
+        lsp = {
+          enabled = true,
+          on_attach = function(client, bufnr)
+            -- the same on_attach function as for your other language servers
+            -- can be ommited if you're using the `LspAttach` autocmd
+          end,
+          actions = true,
+          completion = true,
+          hover = true,
         },
+        --completion = {
+        --  cmp = {
+        --    enabled = true
+        --  },
+        --},
       }
       require("cmp").setup.buffer({
         sources = { { name = "crates" } }
