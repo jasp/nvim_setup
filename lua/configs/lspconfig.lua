295e2c84d61485ee5a8cc612ce2894cec75d6f51
Jakob Skov-Pedersen <jasp@devicelab.dk>
2026-02-28 17:29:48 +0100

    Don't enable rust_analyzer in lsp config
    Missed a warning here: https://github.com/mrcjkb/rustaceanvim about not
    loading rust analyzer manually. This meant all suggestions showed twice.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,6 +2,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "csharp_ls", "rust_analyzer", "slint_lsp", "roslyn" }
+local servers = { "html", "cssls", "clangd", "csharp_ls", "slint_lsp", "roslyn" }
 vim.lsp.enable(servers)
 vim.lsp.inlay_hint.enable()
